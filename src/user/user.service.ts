29db330ff06047a11731a209e5a6ad4fe5c5f844
Mihran <33598667+Mirokh@users.noreply.github.com>
2022-11-13 03:28:52 +0400

    Revert "Revert "addin search findById""

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,4 +6,7 @@ export class UserService {
     public findAll() {
         return userMock;
     }
+    public findOne(id) {
+        return userMock.filter((user) => user.id === Number(id))
+    }
 }
